51d812959c564cee3f0d20a57e540fc96240954a
PRERIT0007 <preritg050@gmail.com>
2023-10-01 02:05:20 +0530

    added concurrently and fixed bugs

--- server/database/db.js
+++ server/database/db.js
@@ -4,8 +4,6 @@ const DBConnection = async () => {
 
     const MONGODB_URI = `mongodb+srv://preritg050:Omsairam7@file-sharing.9y6ijxf.mongodb.net/`;
     try {
-
-        console.log{'hi'}
         await mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
         console.log('Database connected successfully');
     } catch (error) {

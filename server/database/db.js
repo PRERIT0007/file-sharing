06e406e17b76f14234e3d7505261ae6fab81c9b5
PRERIT0007 <preritg050@gmail.com>
2023-10-01 02:08:08 +0530

    .

--- server/database/db.js
+++ server/database/db.js
@@ -4,6 +4,7 @@ const DBConnection = async () => {
 
     const MONGODB_URI = `mongodb+srv://preritg050:Omsairam7@file-sharing.9y6ijxf.mongodb.net/`;
     try {
+        
         await mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
         console.log('Database connected successfully');
     } catch (error) {
